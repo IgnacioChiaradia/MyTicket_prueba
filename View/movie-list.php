56c631dd4acda29b9835490d7913eba430d5c4d5
RodrigoSoto <cotomdq@hotmail.com>
2019-10-15 01:17:46 -0300

    hay que modificar el fondo de las pelis para que sea todo negro asi se ve bien, buscar imagen por defecto para las que no tienen. Lo demas quedo bien

--- View/movie-list.php
+++ View/movie-list.php
@@ -1,34 +1,49 @@
-  <?php  include(VISTA . 'navbar.php') ?>
+  <?php include(VISTA . 'navbar.php') ?>
 
-<main class="py-5">
-     <section id="listado" class="mb-5">
-          <div class="container">
-               <h2 style="color:white" class="mb-4">Listado de peliculas</h2>
-               <table class="table bg-light-alpha"  border = 1>
-                    <thead>
-                         <th style="color:white">Titulo</th>
-                         <th style="color:white">Popularidad</th>
-                         <th style="color:white">Votos</th> <!-- Vote_count -->
-                         <th style="color:white">id</th>
-                    </thead>
-                    <tbody>
-                         <?php
-                              if(!empty($arrayMovies)){
-                                   foreach($arrayMovies as $movie){
-                                        ?>
-                                             <tr>
-                                                  <td style="color:white"><?php echo $movie->getTitle(); ?></td>
-                                                  <td style="color:white"><?php echo $movie->getVote_count(); ?></td>
-                                                  <td style="color:white"><?php echo $movie->getVote_count(); ?></td>
-                                                  <td style="color:white"><?php echo $movie->getId(); ?></td>
-                                             </tr>
-                                        <?php
-                                   }
-                              }
-                         ?>
-                         </form>
-                    </tbody>
-               </table>
-          </div>
-     </section>
-</main>
+  <main class="my-5">
+       <section class="mb-5">
+            <div class="container-fluid">
+                 <div class="row-fluid">
+                      <h2 class="display-2 text-center text-light ">Peliculas en proyeccion</h2>
+                 </div>
+            </div>
+       </section>
+  </main>
+  <?php
+     if (!empty($arrayMovies)) {
+          foreach ($arrayMovies as $movie) {
+               ?>
+
+            <div class="container-fluid">
+                 <div class="row">
+                      <div class="col-4  my-5">
+                           <img src="https://image.tmdb.org/t/p/w500/<?php echo $movie->getPoster_path() ?>" alt="Imagen pelicula" class="img-fluid rounded">
+                      </div>
+                      <div class="col-8  my-5">
+                           <table class="table">
+                                <thead class="text-light">
+                                     <th>Titulo: <?php echo $movie->getTitle() ?></th>
+                                     <th>Idioma: <?php echo $movie->getOriginal_language() ?></th>
+                                     <th>Fecha: <?php echo $movie->getRelease_date() ?></th>
+                                     <th>Calificacion: <?php echo $movie->getVote_average() ?></th>
+                                </thead>
+                                
+                           </table>
+                           <div class="text-light"><?php echo $movie->getOverview() ?></div>
+                      </div>
+                 </div>
+            </div>
+
+  <?php
+          }
+     }
+     ?>
+
+
+
+
+
+
+
+
+
